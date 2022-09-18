b19030a489fdec2695f7170697f997a0c4d1f009
Nikola Ivanov <nikola.ivanov.wd@gmail.com>
2022-09-18 20:12:06 +0200

    Add support to the _slider.latte component to accept an instance of the WP_Query as the alternative to the array

--- views/temp/views-templates-front-page.latte--c1b3f7dea5.php
+++ views/temp/views-templates-front-page.latte--c1b3f7dea5.php
@@ -14,18 +14,15 @@ final class Templatec1b3f7dea5 extends Latte\Runtime\Template
 		$this->createTemplate(tr_part('todo-remove-examples'), $this->params, 'include')->renderToContentType('html') /* line 3 */;
 		echo '
 
-
-
-
 <div class="content">
 	';
-		echo LR\Filters::escapeHtmlText(the_content()) /* line 10 */;
+		echo LR\Filters::escapeHtmlText(the_content()) /* line 7 */;
 		echo '
 </div>
 
 
 ';
-		$this->createTemplate(tr_view_path('/layout/footer'), $this->params, 'include')->renderToContentType('html') /* line 14 */;
+		$this->createTemplate(tr_view_path('/layout/footer'), $this->params, 'include')->renderToContentType('html') /* line 11 */;
 		return get_defined_vars();
 	}
 
